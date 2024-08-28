d1a264f4808ace07dbb0cb737c4511a78108c9e8
Binta Diakhate <binta9619@gmail.com>
2024-08-28 12:11:31 +0000

    ajout du fichier webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',  // Point d'entrée de votre application
+  output: {
+    filename: 'bundle.js',  // Nom du fichier de sortie
+    path: path.resolve(__dirname, 'dist'),  // Répertoire de sortie
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
